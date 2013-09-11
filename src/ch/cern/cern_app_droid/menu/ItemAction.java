95ead7fbd0f94df8ead73cf4e5ba6d0d04c29c2e
Luke Wasylkowski <l.wasyl@gmail.com>
2013-09-11 19:27:00 +0200

    Removing everything to set up workspace again

--- src/ch/cern/cern_app_droid/menu/ItemAction.java
+++
@@ -1,9 +0,0 @@
-package ch.cern.cern_app_droid.menu;
-
-import android.support.v4.app.Fragment;
-
-public interface ItemAction {
-
-	public Fragment getFragment();
-	public void onReselected();
-}
--- src/ch/cern/cern_app_droid/rss/RssFeedFragmentTablet.java
+++
@@ -1,8 +0,0 @@
-package ch.cern.cern_app_droid.rss;
-
-
-public class RssFeedFragmentTablet extends android.app.Fragment {
-	
-	
-
-}
